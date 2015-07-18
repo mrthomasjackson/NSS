3fab7fbdff90f524c6693ecd52ede16239b776fc
mrthomasjackson <tjackson@TFJs-MacBook-Air.local>
2015-07-18 11:52:57 -0400

    Added Assignment Bash

---
+++ ExerciseFiles/my.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This is a basic bash script
+#greeting ="hello";
+
+#Echo ($greeting / hello \(planet\)!
+d=2
+e=$((d+2))
+echo $e
+((e++))
+echo $e
+((e--))
+echo $e
+echo
+((e+5))
+echo $e
+
